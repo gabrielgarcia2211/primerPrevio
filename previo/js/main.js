0d32c9adbbefece640099d22e8ddcce02e0514c4
gabriel <gabrielarturogq@ufps.edu.co>
2020-05-07 08:53:15 +0200

    ultima entrega

--- previo/js/main.js
+++ previo/js/main.js
@@ -1,4 +1,16 @@
 
+$( document ).ready(function() {
+    console.log("hola");
+
+    $(".registrar").click(function(){
+        $(".inferior").load("registro.html");
+        console.log("hola");
+    });
+
+});
 
 
+function carga(){
+    $(".informacion").load("mas.html");
+}
 
